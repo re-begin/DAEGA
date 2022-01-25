14716fdcb30fe67cfd070c9ffd2f00aea93e0692
re-begin <741656951@qq.com>
2022-01-25 18:41:10 +0800

    update Optimize_DAE_Using_GA.py

--- Optimize_DAE_Using_GA.py
+++ Optimize_DAE_Using_GA.py
@@ -1,22 +1,31 @@
 # Optimization the denoising autoencoder by genetic algorithm for anomaly detection (DAEGA)
 
+from cgi import print_directory
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, f1_score, precision_score,roc_curve
 
-input_size = 5
-hidden_size = 3
+from Dateloader import *
+
+np.random.seed(0)
+
+input_size = 10
+hidden_size = 5
+num_epoch = 1
 
 # 迭代次数
 num_generations = 100
 # 染色体长度
 chr_size = 2*input_size + input_size*hidden_size*2 + hidden_size
 # 每个群落中染色体的数量
-chr_per_pop = 8 
+chr_per_pop = 100 
 # 定义群落规模，(染色体数量，染色体长度).
 population_size = (chr_per_pop, chr_size)
 # 初始化群落.
 new_population = np.random.uniform(low=-1.0, high=1.0, size=population_size)
 # 群落中精英染色体数量
-num_parents_mating = 4 
+num_parents_mating = 30
+
 # 交配得到的下一代基因来自父染色体的概率
 p = 0.5
 # 每条染色变异基因的最大数量（不能大于chr_size）
@@ -100,48 +109,170 @@ def mutation(offspring_crossover):
         offspring_crossover[idx, np.random.randint(0, chr_size, num_mutation)] = random_value
     return offspring_crossover
 
-def train():
+def train(date):
 
-    for generation in range(num_generations):
+    # batch_avg_fitness_list = []
 
-        x = [0.2,0.4,0.9,0.1,0.5]
+    for batch in range(num_epoch):
 
-        # 计算适应度
-        fitness = col_pop_fitness(x, new_population)
+        # batch_fitness = []
 
-        # 在群落中选择适应度最小的染色体做为精英.
-        parents = select_mating_pool(new_population, fitness, num_parents_mating)
+        for idx, x in enumerate(date):
 
-        # 通过交配生成下一代.
-        offspring_crossover = crossover(parents, offspring_size=(population_size[0]-parents.shape[0], chr_size))
-
-        # 基因变异.
-        offspring_mutation = mutation(offspring_crossover)
-
-        # 得到下一代.
-        new_population[0:parents.shape[0], :] = parents
-        new_population[parents.shape[0]:, :] = offspring_mutation
-
-        # The best result in the current iteration.
-        if generation%20 == 0: 
-            print("Generation : ", generation)
+            # 计算适应度
             fitness = col_pop_fitness(x, new_population)
-            print("Best result : ", np.min(fitness))
 
-    # Getting the best solution after iterating finishing all generation
-    # At first, the fitness is calculated for each solution in the final generation
-    fitness = col_pop_fitness(x, new_population)
+            # 在群落中选择适应度最小的染色体做为精英.
+            parents = select_mating_pool(new_population, fitness, num_parents_mating)
 
-    # Then return the index of the solution corresponding the best fitness
-    best_match_idx = np.where(fitness == np.min(fitness))[0][0]
-    best_chromosome = new_population[best_match_idx, :]
-    # print("Best_match_idx : ", best_match_idx)
-    print("Best solution : ", best_chromosome)
-    print("Best solution fitness : ", fitness[best_match_idx])
+            # 通过交配生成下一代.
+            offspring_crossover = crossover(parents, offspring_size=(population_size[0]-parents.shape[0], chr_size))
 
+            # 基因变异.
+            offspring_mutation = mutation(offspring_crossover)
+
+            # 得到下一代.
+            new_population[0:parents.shape[0], :] = parents
+            new_population[parents.shape[0]:, :] = offspring_mutation
+
+            fitness = col_pop_fitness(x, new_population)
+            # batch_fitness.append(np.min(fitness))
+
+            if idx%5000 == 0: 
+                print("Generation : ", idx)
+                print("Best result : ", np.min(fitness))
+
+        # Getting the best solution after iterating finishing all generation
+        # At first, the fitness is calculated for each solution in the final generation
+        
+        fitness = col_pop_fitness(x, new_population)
+        # batch_avg_fitness = np.mean(batch_fitness)
+        # batch_avg_fitness_list.append(batch_avg_fitness)
+
+        # Then return the index of the solution corresponding the best fitness
+        best_match_idx = np.where(fitness == np.min(fitness))[0][0]
+        best_chromosome = new_population[best_match_idx, :]
+        # print("Best_match_idx : ", best_match_idx)
+        print("Best solution : ", best_chromosome)
+        print("Best solution fitness : ", fitness[best_match_idx])
+
+    # plt.plot(range(len(batch_avg_fitness_list)), batch_avg_fitness_list)
+    # plt.show()
     return best_chromosome
 
+def predict(traindate, test_date):
+
+    best_chromosome = train(date=traindate)
+
+    test_date_after = np.array([[]]*input_size).T
+    # print(test_date_after.shape)
+    for i in test_date:
+        new_i = DAE(best_chromosome, i)
+        new_i = np.array(new_i).reshape(1,10)
+        test_date_after = np.vstack([test_date_after, new_i])   
+
+    print("test_date_after.shape: ", test_date_after.shape)
+
+    return test_date_after
+
+def distance(x, y):
+    distance = []
+    for i, j in zip(x, y):
+        dist = np.sqrt(np.sum(np.square(i - j)))
+        distance.append(dist)
+    return distance
+
+def datetrans(distance_arr, label_thre, threshold):
+    distance_arr = np.array(distance_arr)
+    label = np.array([0]*len(distance_arr))
+    y = np.array([0]*len(distance_arr))
+
+    label[distance_arr < label_thre] = 1
+    y[distance_arr < threshold] = 1
+    return label, y
+
+def getmatrix(x, y):
+    acc = accuracy_score(x,y)
+    f1 = f1_score(x, y)
+    pre = precision_score(x, y)
+    print("accuracy is: ", acc)
+    print("f1 score is: ", f1)
+    print("precision is: ", pre)
 
 if __name__ == '__main__':
-    best_chromosome = train()
+    # 台区1原始数据-时间窗口split前数据
+    testdate_huifu1_1 = redate_local(testdate_1_1, 10, num_user1)
+    testdate_huifu1_2 = redate_local(testdate_1_2, 10, num_user1)
+    testdate_huifu1_3 = redate_local(testdate_1_3, 10, num_user1)
+    testdate_huifu1_4 = redate_local(testdate_1_4, 10, num_user1)
+    # 台区2原始数据-时间窗口split前数据
+    testdate_huifu2_1 = redate_local(testdate_2_1, 10, num_user2)
+    testdate_huifu2_2 = redate_local(testdate_2_2, 10, num_user2)
+    testdate_huifu2_3 = redate_local(testdate_2_3, 10, num_user2)
+    testdate_huifu2_4 = redate_local(testdate_2_4, 10, num_user2)
     
+    # 台区1预测数据-时间窗口split后数据
+    test_date_after1_1 = predict(traindate_1_1, testdate_1_1)
+    test_date_after1_2 = predict(traindate_1_2, testdate_1_2)
+    test_date_after1_3 = predict(traindate_1_3, testdate_1_3)
+    test_date_after1_4 = predict(traindate_1_4, testdate_1_4)
+    # 台区2预测数据-时间窗口split后数据
+    test_date_after2_1 = predict(traindate_2_1, testdate_2_1)
+    test_date_after2_2 = predict(traindate_2_2, testdate_2_2)
+    test_date_after2_3 = predict(traindate_2_3, testdate_2_3)
+    test_date_after2_4 = predict(traindate_2_4, testdate_2_4)
+    
+    # 台区1预测数据-恢复时间窗口split前格式
+    testdate_predict1_1 = redate_local(test_date_after1_1, 10, num_user1)
+    testdate_predict1_2 = redate_local(test_date_after1_2, 10, num_user1)
+    testdate_predict1_3 = redate_local(test_date_after1_3, 10, num_user1)
+    testdate_predict1_4 = redate_local(test_date_after1_4, 10, num_user1)
+
+    # 台区2预测数据-恢复时间窗口split前格式
+    testdate_predict2_1 = redate_local(test_date_after2_1, 10, num_user2)
+    testdate_predict2_2 = redate_local(test_date_after2_2, 10, num_user2)
+    testdate_predict2_3 = redate_local(test_date_after2_3, 10, num_user2)
+    testdate_predict2_4 = redate_local(test_date_after2_4, 10, num_user2)
+
+    # 台区1-数据合并还原为未dateprocess后格式（n, 4）
+    test1_origin_col1 = testdate_huifu1_1.reshape(-1,1)
+    test1_origin_col2 = testdate_huifu1_2.reshape(-1,1)
+    test1_origin_col3 = testdate_huifu1_3.reshape(-1,1)
+    test1_origin_col4 = testdate_huifu1_4.reshape(-1,1)
+
+    test1_origin = np.hstack([test1_origin_col1, test1_origin_col2, test1_origin_col3, test1_origin_col4])
+    print("test1_origin.shape", test1_origin.shape)
+
+    test1_predict_col1 = testdate_predict1_1.reshape(-1,1)
+    test1_predict_col2 = testdate_predict1_2.reshape(-1,1)
+    test1_predict_col3 = testdate_predict1_3.reshape(-1,1)
+    test1_predict_col4 = testdate_predict1_4.reshape(-1,1)
+
+    test1_predict = np.hstack([test1_predict_col1, test1_predict_col2, test1_predict_col3, test1_predict_col4])
+    print("test1_predict.shape", test1_predict.shape)
+
+    # 台区2-数据合并还原为未dateprocess后格式（n, 4）
+    test2_origin_col1 = testdate_huifu2_1.reshape(-1,1)
+    test2_origin_col2 = testdate_huifu2_2.reshape(-1,1)
+    test2_origin_col3 = testdate_huifu2_3.reshape(-1,1)
+    test2_origin_col4 = testdate_huifu2_4.reshape(-1,1)
+
+    test2_origin = np.hstack([test2_origin_col1, test2_origin_col2, test2_origin_col3, test2_origin_col4])
+    print("test2_origin.shape", test2_origin.shape)
+
+    test2_predict_col1 = testdate_predict2_1.reshape(-1,1)
+    test2_predict_col2 = testdate_predict2_2.reshape(-1,1)
+    test2_predict_col3 = testdate_predict2_3.reshape(-1,1)
+    test2_predict_col4 = testdate_predict2_4.reshape(-1,1)
+
+    test2_predict = np.hstack([test2_predict_col1, test2_predict_col2, test2_predict_col3, test2_predict_col4])
+    print("test2_predict.shape", test2_predict.shape)
+
+    distance1 = distance(test1_origin, test1_predict)
+    distance2 = distance(test2_origin, test2_predict)
+
+    label1, y1 = datetrans(distance1, 1.4, 5)
+    label2, y2 = datetrans(distance2, 0.55, 5)
+  
+    getmatrix(label1, y1)
+    getmatrix(label2, y2)  
